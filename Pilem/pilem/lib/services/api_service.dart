9c997ae7bde340e079dd1228a45d51c0cded8d10
muhammadtegarramadhani2327240067-debug <muhammadtegarramadhani_2327240067@mhs.mdp.ac.id>
2026-03-03 13:58:43 +0700

    update
    membuat project website film

---
+++ Pilem/pilem/lib/services/api_service.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = 'be9855f5efaa622080b485e3025dba2d';
+
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+
+  // getTrendingMovies
+  // getPopularMovies
+}
